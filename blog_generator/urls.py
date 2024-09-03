4bb16b4872e144fe3259eabb965aec09dff3ed4c
DucLap <rumbonaruson@gmail.com>
2024-09-04 00:24:52 +0700

    Convert all static methods to function

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserAuthor.index, name='index'),
-    path('login', views.UserAuthor.login, name='login'),
-    path('signup', views.UserAuthor.signup, name='signup'),
-    path('logout', views.UserAuthor.logout, name='logout'),
+    path('', views.index, name='index'),
+    path('login', views.user_login, name='login'),
+    path('signup', views.user_signup, name='signup'),
+    path('logout', views.user_logout, name='logout'),
+    path('generate-blog', views.generate_blog, name='generate-blog'),
 ]
--- blog_generator/views.py
+++ blog_generator/views.py
@@ -2,56 +2,73 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
 
 
 # Create your views here.
-class UserAuthor:
-    @login_required(login_url="login")
-    @staticmethod
-    def index(request):
-        return render(request, "index.html")
+@login_required(login_url="login")
+def index(request):
+    return render(request, "index.html")
 
-    @staticmethod
-    def login(request):
-        if request.method == "POST":
-            username = request.POST["username"]
-            password = request.POST["password"]
 
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
+# @login_required(login_url="login")
+@csrf_exempt  # this is avoiding required csrf_token
+def generate_blog(request):
+    """Automated generate a blog article"""
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)  # get body of json file, which fetched in script in index.html
+            ytb_link = data["link"]
+        except (KeyError, json.JSONDecodeError):
+            return JsonResponse({"error": "Invalid data sent."}, status=400)
+
+
+    else:
+        return JsonResponse({"error": "Method is not valid."}, status=405)  # prevent user to use different methods except POST
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            error_message = "Invalid username or password"
+            return render(request, "login.html", {"error_message": error_message})
+
+    return render(request, "login.html")
+
+
+def user_signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm-password"]
+
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
                 login(request, user)
                 return redirect("/")
-            else:
-                error_message = "Invalid username or password"
-                return render(request, "login.html", {"error_message": error_message})
-
-        return render(request, "login.html")
-
-    @staticmethod
-    def signup(request):
-        if request.method == "POST":
-            username = request.POST["username"]
-            email = request.POST["email"]
-            password = request.POST["password"]
-            confirm_password = request.POST["confirm-password"]
-
-            if password == confirm_password:
-                try:
-                    user = User.objects.create_user(username, email, password)
-                    user.save()
-                    login(request, user)
-                    return redirect("/")
-                except Exception as ex:  # maybe this account have been taken
-                    error_message = "Error creating account"
-                    return render(request, "signup.html", {"error_message": error_message})
-
-            else:
-                error_message = "Passwords don't match!"
+            except Exception as ex:  # maybe this account have been taken
+                error_message = "Error creating account"
                 return render(request, "signup.html", {"error_message": error_message})
 
-        return render(request, "signup.html")
+        else:
+            error_message = "Passwords don't match!"
+            return render(request, "signup.html", {"error_message": error_message})
 
-    @staticmethod
-    def logout(request):
-        logout(request)
-        return redirect("/login")
+    return render(request, "signup.html")
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("/login")
